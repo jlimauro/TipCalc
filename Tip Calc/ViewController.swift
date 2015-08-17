617f06de605b10d02d3c0eed771680925860d48b
Jeffrey Limauro <jlimauro@gmail.com>
2015-08-16 20:50:50 -0400

    First check in.

--- Tip Calc/ViewController.swift
+++ Tip Calc/ViewController.swift
@@ -8,18 +8,84 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var billAmount: UITextField!
+    
+    @IBOutlet weak var tipPercent: UITextField!
+    
+    @IBOutlet weak var tipStepper: UIStepper!
+    
+    @IBOutlet weak var tipAmount: UILabel!
+    
+    @IBOutlet weak var totalAmount: UILabel!
+    
+    @IBAction func tapAction(sender: AnyObject) {
+        //This tapping of view will hid num pad
+        billAmount.resignFirstResponder()
+    }
+    var currentValue : Double = 0.0
+    var subTotal : Double = 0.0
+    var calcTip : Double = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tipStepper.wraps = true
+        tipStepper.autorepeat = true
+        tipPercent.text = "\(20)"
+        
+        currentValue = (tipPercent.text as NSString).doubleValue
+        tipStepper.value = currentValue
+
     }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        billAmount.resignFirstResponder()
+        return true;
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func calculateTip(sender: UITextField) {
+        
+        if (billAmount.text != nil && tipPercent.text != nil)
+        {
+            displayResult()
+        }
+        
+    }
+    
+    @IBAction func stepperValueChanged(sender: UIStepper) {
+        
+       
+        tipPercent.text = Int(sender.value).description
+        
+        currentValue = (tipPercent.text as NSString).doubleValue
+        
+        if (billAmount.text != nil && tipPercent.text != nil)
+        {
+            displayResult()
+        }
+        
+    }
+    
+    func displayResult()
+    {       
+        
+        subTotal = (billAmount.text as NSString).doubleValue
+        
+        calcTip = subTotal * (currentValue/100)
+        
+        tipAmount.text = String(format: "$%.2f", calcTip)
+        
+        totalAmount.text = String(format: "$%.2f", calcTip + subTotal)
+    }
+    
 }
 
